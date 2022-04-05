67f3988516155e6b0a35a687c423c84319e1eede
Mario Balan <mario.balan@gmail.com>
2022-04-04 20:21:50 -0400

    Change Cohesion and Coupling (post-refactoring)

--- python/patterns/cohesion_and_coupling.py
+++ python/patterns/cohesion_and_coupling.py
@@ -1,13 +1,66 @@
 import string
 import random
 
+
+class VehicleInformation:
+    brand: str
+    electric: bool
+    catalogue_price: int
+
+    def __init__(self, brand, electric, catalogue_price):
+        self.brand = brand
+        self.electric = electric
+        self.catalogue_price = catalogue_price
+
+    def determine_tax_percentage(self):
+        return 0.05 if self.electric == True else 0.02
+
+    def calculate_tax(self):
+        return self.determine_tax_percentage() * self.catalogue_price
+
+    def print(self):
+        print(f"Brand: {self.brand}")
+        print(f"Payable Tax: {self.calculate_tax()}")
+
+
+class Vehicle:
+    id: str
+    license_plate: str
+    info: VehicleInformation
+
+    def __init__(self, id, license_plate, info):
+        self.id = id
+        self.license_plate = license_plate
+        self.info = info
+
+    def print(self):
+        print(f"Id: {self.id}")
+        print(f"License Plate: {self.license_plate}")
+        self.info.print()
+
+
 class VehicleRegistry:
+    vehicleInformation = {}
+
+    def __init__(self): # I'm just initializing the previous vehicles to simulate a database
+        self.add_vehicle_info("Tesla Model 3", True, 60000)
+        self.add_vehicle_info("Volkswagen ID3", True, 35000)
+        self.add_vehicle_info("BMW 5", False, 45000)
+        self.add_vehicle_info("Volkswagen Fusca", False, 15000)
+
+    def add_vehicle_info(self, brand, electric, catalogue_price):
+        self.vehicleInformation[brand] = VehicleInformation(brand, electric, catalogue_price)
 
     def generate_vehicle_id(self, length):
         return ''.join(random.choices(string.ascii_uppercase, k=length))
 
     def generate_vehicle_license(self, id):
         return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
+
+    def create_vehicle(self, brand):
+        vehicle_id = self.generate_vehicle_id(12)
+        license_plate = self.generate_vehicle_license(vehicle_id)
+        return Vehicle(vehicle_id, license_plate, self.vehicleInformation[brand])
 
 
 class Application:
@@ -16,36 +69,10 @@ class Application:
         # create a registry instance
         registry = VehicleRegistry()
 
-        # generate a vehicle id of length 12
-        vehicle_id = registry.generate_vehicle_id(12)
+        # create a vehicle
+        return registry.create_vehicle(brand)
 
-        # now generate a license plate for the vehicle
-        # using the first two characters of the vehicle id
-        license_plate = registry.generate_vehicle_license(vehicle_id)
-
-        # compute the catalogue price
-        catalogue_price = 0
-        if brand == "Tesla Model 3":
-            catalogue_price = 60000
-        elif brand == "Volkswagen ID3":
-            catalogue_price = 35000
-        elif brand == "BMW 5":
-            catalogue_price = 45000
-
-        # compute the tax percentage (default 5% of the catalogue price, except for electric cars where it is 2%)
-        tax_percentage = 0.05
-        if brand == "Tesla Model 3" or brand == "Volkswagen ID3":
-            tax_percentage = 0.02
-
-        # compute the payable tax
-        payable_tax = tax_percentage * catalogue_price
-
-        # print out the vehicle registration information
-        print("Registration complete. Vehicle information:")
-        print(f"Brand: {brand}")
-        print(f"Id: {vehicle_id}")
-        print(f"License plate: {license_plate}")
-        print(f"Payable tax: {payable_tax}")
 
 app = Application()
-app.register_vehicle("Volkswagen ID3")
+vehicle = app.register_vehicle("Volkswagen Fusca")
+vehicle.print()
